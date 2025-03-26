73b1ade4bd311aec15f329290d26a2d2d78a3b87
dev <devmanojkumar08@gmail.com>
2025-03-26 13:14:06 +1100

    605. Cleaner version

--- 605. Can Place Flowers.py
+++ 605. Can Place Flowers.py
@@ -19,9 +19,25 @@ class Solution:
 
         return placeable >= n
 
+# Cleaner Solution:
+# class Solution:
+#     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+#         count = 0
+#         i = 0
+#         while i < len(flowerbed):
+#             if (flowerbed[i] == 0 and
+#                 (i == 0 or flowerbed[i - 1] == 0) and
+#                 (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+#                 flowerbed[i] = 1
+#                 count += 1
+#                 i += 2  # skip the next spot
+#             else:
+#                 i += 1
+#         return count >= n
+
 
 if __name__ == "__main__":
     sol = Solution()
     flowerbed1 = [1,0,0,0,1,0,0]
     n1 = 2
-    print(sol.canPlaceFlowers(flowerbed1, n1)) 
+    print(sol.canPlaceFlowers(flowerbed1, n1))
