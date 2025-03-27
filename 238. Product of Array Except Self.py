f86910c9140a39908abf3332b026ebb50a2546b5
dev <devmanojkumar08@gmail.com>
2025-03-27 17:17:20 +1100

    more efficient method

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,19 +1,41 @@
-import operator
+# combines prefix and suffix sums through 2 passes of array
+# naturally considers '0' cases
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        for i in range(len(nums)):
-            if nums[i]:
-                prod = prod * nums[i]
+        n = len(nums)
+        res = [1] * n
 
-        num_zeros = nums.count(0)
-        if num_zeros > 1:
-            return [0] * len(nums)
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
 
-        if num_zeros == 1:
-            return [0 if nums[i] else prod for i in range(len(nums))]
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
 
-        else:
-            return [prod // nums[i] for i in range(len(nums))]
+        return res
+
+
+# not efficient
+# import operator
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         prod = 1
+#         for i in range(len(nums)):
+#             if nums[i]:
+#                 prod = prod * nums[i]
+
+#         num_zeros = nums.count(0)
+#         if num_zeros > 1:
+#             return [0] * len(nums)
+
+#         if num_zeros == 1:
+#             return [0 if nums[i] else prod for i in range(len(nums))]
+
+#         else:
+#             return [prod // nums[i] for i in range(len(nums))]
+
 
 
